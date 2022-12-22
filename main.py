fdc9e9a216302156d37f1fc89bf1a590f16a89b2
Alla Katkova <allakatkova@bk.ru>
2022-12-22 22:45:02 +0500

    Add connect SQLAlchemy

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
+import sqlalchemy as sq
+from sqlalchemy.orm import declarative_base, relationship, sessionmaker
+
+NAME_DATABASE = 'sales_books'
+USER_DATABASE = 'postgres'
+PASS_USER_DATABASE = 'postgres'
+HOST_NAME = 'localhost'
+PORT_HOST = '5432'
+
+Base = declarative_base()
+
+
+def create_tables(engine):
+    # Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
 
 
 if __name__ == '__main__':
-    print('ghb')
+
+    DSN = "postgresql://" + USER_DATABASE + ":" + PASS_USER_DATABASE + \
+        "@" + HOST_NAME + ":" + PORT_HOST + "/" + NAME_DATABASE
+    engine = sq.create_engine(DSN)
+
+    print(sqlalchemy.__version__)
